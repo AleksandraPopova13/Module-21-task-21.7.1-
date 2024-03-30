a4b2147013a520069d772a4f5b54552eb4583c7e
Aleksandra <157876297+AleksandraPopova13@users.noreply.github.com>
2024-03-30 15:55:26 +0400

    Add files via upload

---
+++ task 21.7.1/test_pet_house.py
@@ -0,0 +1,6 @@
+from pet_house import Cat
+cat1 = Cat('Барон', 'мальчик', 2)
+cat2 = Cat('Сэм', 'мальчик', 2)
+
+print('1) Имя:', cat1.name, ',', 'пол:', cat1.gender, ',' , 'возраст:', cat1.age)
+print('2) Имя:', cat2.name, ',', 'пол:', cat2.gender, ',' , 'возраст:', cat2.age)
